3643dc6d8d2b43806c00e2db7c2d9013686de180
rouge2692 <rouge2692@gmail.com>
2024-05-03 14:00:04 -0700

    server connection attempt 2

--- Clean-server/config.py
+++ Clean-server/config.py
@@ -1,7 +1,7 @@
 import os
 
 MONGODB_SETTINGS = {"db": "SweepCollect", "host": "localhost", "port": 27017}
-SQLALCHEMY_DATABASE_URI = "postgres://postgres.yzehfauuthhnscfnqwfl:@rrM785123@aws-0-ca-central-1.pooler.supabase.com:5432/postgres"
+SQLALCHEMY_DATABASE_URI = "postgresql://postgres.yzehfauuthhnscfnqwfl:@rrM785123@aws-0-ca-central-1.pooler.supabase.com:5432/postgres"
 
 # SQLALCHEMY_DATABASE_URI = "postgresql://postgres:am785123@localhost/sweep"
 

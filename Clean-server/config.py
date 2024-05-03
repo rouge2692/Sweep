e1b9b8d01d2ef5f7944c3425405ec5c045ecab50
rouge2692 <rouge2692@gmail.com>
2024-05-03 01:29:11 -0700

    added supabase uri

--- Clean-server/config.py
+++ Clean-server/config.py
@@ -1,7 +1,8 @@
 import os
 
 MONGODB_SETTINGS = {"db": "SweepCollect", "host": "localhost", "port": 27017}
+SQLALCHEMY_DATABASE_URI = "postgres://postgres.yzehfauuthhnscfnqwfl:@rrM785123@aws-0-ca-central-1.pooler.supabase.com:5432/postgres"
+
 # SQLALCHEMY_DATABASE_URI = "postgresql://postgres:am785123@localhost/sweep"
-SQLALCHEMY_DATABASE_URI = "postgres://postgres.yzehfauuthhnscfnqwfl:[YOUR-PASSWORD]@aws-0-ca-central-1.pooler.supabase.com:5432/postgres"
 
 # user=postgres.yzehfauuthhnscfnqwfl password=[YOUR-PASSWORD] host=aws-0-ca-central-1.pooler.supabase.com port=5432 dbname=postgres
